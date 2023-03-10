f00dcd0cc478f796658bf5d7bd14b0eab6e4c7d3
tscabral1 <44780117+tscabral1@users.noreply.github.com>
2023-03-10 07:43:27 -0300

    Create install1.sh

---
+++ install1.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+
+###############################################
+#Install Openbox and basic apps on Arch Linux #
+#or Arch Linux based distros.                 #
+###############################################
+
+#Configure touchpad tap
+sudo echo 'Section "InputClass"
+Identifier "touchpad"
+Driver "libinput"
+  MatchIsTouchpad "on"
+  Option "Tapping" "on"
+  Option "ClickMethod" "buttonareas"
+EndSection' >> /etc/X11/xorg.conf.d/30-touchpad.conf;
+
+#Autologin
+sudo mkdir /etc/systemd/system/getty@tty1.service.d;
+sudo echo '[Service]
+Type=simple
+ExecStart=
+ExecStart=-/sbin/agetty --autologin thiago --noclear %I 38400 linux' >> /etc/systemd/system/getty@tty1.service.d/override.conf
